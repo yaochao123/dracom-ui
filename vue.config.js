db963bcc9c997f2677a7bb57bcf5df206516d751
yc <>
2020-05-15 09:15:52 +0800

    配置文件代码格式化

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,11 @@ module.exports = {
   parallel: false,
   productionSourceMap: false,
   pages: {
-    index: { entry: 'examples/src/main.ts', template: 'public/index.html', filename: 'index.html' }
+    index: {
+      entry: 'examples/src/main.ts',
+      template: 'public/index.html',
+      filename: 'index.html'
+    }
   },
   chainWebpack(config) {
     config.resolve.alias.set('@', resolve('./examples/src'))
