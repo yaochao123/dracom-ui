078542cbc7e867fcb5e158abbff49b7b7a3ece2a
yc <>
2020-06-02 09:29:51 +0800

    rem基本值修改

--- vue.config.js
+++ vue.config.js
@@ -47,7 +47,7 @@ module.exports = {
       postcss: {
         plugins: [
           require('postcss-px2rem')({
-            remUnit: 75
+            remUnit: 37.5
           })
         ]
       }
