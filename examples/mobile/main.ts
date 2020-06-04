03c532b0f665f703d4a9c4b9d129d4458d19561a
YaoChao <yaochao@hzdracom.com>
2020-06-04 11:34:09 +0800

    iframe修改主题色

--- examples/mobile/main.ts
+++ examples/mobile/main.ts
@@ -8,14 +8,38 @@ import '@src/style/_animation.scss'
 import Dracom from '../../src/index'
 // css变量兼容
 import 'mutationobserver-shim'
-import cssVars from "css-vars-ponyfill"
+import cssVars from 'css-vars-ponyfill'
 cssVars({
   watch: true
-});
+})
 
 Vue.config.productionTip = false
 
 Vue.use(Dracom)
+
+/**
+ * 父页面主题色修改
+ */
+window.onmessage = (e: any) => {
+  const data = e.data
+  if (
+    data !== 'undefined' &&
+    data.type !== 'webpackOk' &&
+    data.type !== 'webpackErrors' &&
+    data.type !== 'webpackInvalid'
+  ) {
+    let style = document.createElement('style')
+    style.type = 'text/css'
+    style.dataset.type = 'theme'
+    style.appendChild(document.createTextNode(data))
+    let head = document.querySelector('head')
+    let finnalStyle = head?.lastChild as HTMLStyleElement
+    if (finnalStyle.dataset.type === 'theme') {
+      head?.removeChild(finnalStyle)
+    }
+    head?.appendChild(style)
+  }
+}
 
 new Vue({
   router,
