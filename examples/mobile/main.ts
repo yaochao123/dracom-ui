f315ea1be836c89fc0a677c736a2f03ff048f37d
姚超 <yaochao@hzdracom.com>
2020-06-08 15:04:20 +0800

    配置修改

--- examples/mobile/main.ts
+++ examples/mobile/main.ts
@@ -4,7 +4,7 @@ import App from './App.vue'
 import '@/style/reset.css'
 import '@src/style/variables.css'
 import Dracom from 'dracom-ui'
-import 'dracom-ui/lib/index/style.css'
+// import 'dracom-ui/lib/index/style.css'
 // import Dracom from '../../src/index'
 
 // css变量兼容
