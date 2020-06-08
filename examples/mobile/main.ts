ae25ca9c853bc776343be67f02d90e1879f68abd
姚超 <yaochao@hzdracom.com>
2020-06-08 18:01:41 +0800

    修改

--- examples/mobile/main.ts
+++ examples/mobile/main.ts
@@ -4,9 +4,7 @@ import App from './App.vue'
 import '@/style/reset.css'
 import '@src/style/variables.css'
 import 'dracom-ui/lib/index/style.css'
-// import Dracom from 'dracom-ui'
-import Dracom from '../../src/index'
-console.log(Dracom)
+import Dracom from 'dracom-ui'
 
 // css变量兼容
 import 'mutationobserver-shim'
